e2317bb61acd01cd34400b2d93789f2e4d25160b
yokesh7373@gmail.com <gitHub@1>
2022-12-15 14:48:14 +0530

    ViewBinding setup in activity

--- app/src/main/java/com/training/simpleviewbinding/MainActivity.java
+++ app/src/main/java/com/training/simpleviewbinding/MainActivity.java
@@ -3,12 +3,20 @@ package com.training.simpleviewbinding;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.training.simpleviewbinding.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding activityMainBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        activityMainBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        View rootView = activityMainBinding.getRoot();
+        setContentView(rootView);
+//        setContentView(R.layout.activity_main);
     }
 }
